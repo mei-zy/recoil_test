b3c86b25793eb4c0cc089a5530a01a6d89bb45d6
mei-zy <alswlkku@ajou.ac.kr>
2023-08-31 18:57:47 +0900

    remove: remove contextAPI code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import ItemEditSection from "./components/ItemEdit";
 import ItemList from "./components/ItemList";
-import APIContextProvider from "./contextAPI/apiContext";
 
 function App() {
   return (
-    <APIContextProvider>
-      <div className="App">
-        <section style={{ display: "flex", gap: 30 }}>
-          <ItemList />
-          <ItemEditSection />
-        </section>
-      </div>
-    </APIContextProvider>
+    <div className="App">
+      <section style={{ display: "flex", gap: 30 }}>
+        <ItemList />
+        <ItemEditSection />
+      </section>
+    </div>
   );
 }
 
